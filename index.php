64af8796cf8be53541f549ac1cb2f7830b0deceb
scottHargus <scotteh@mydigitalhome.net>
2018-01-01 09:04:48 -0600

    Added HTTP 301 header
    Added 301 permanent redirect

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+header( “HTTP/1.1 301 Moved Permanently” );
 header("Location: http://www.illinoisfirejuniors.com"); /* Redirect browser */
 
 /* Make sure that code below does not get executed when we redirect. */
